1eb18f6781f80c6c814af97a5de673964b94cf13
Bartek1817 <bartlomiejplonka@gmail.com>
2016-11-04 22:28:45 +0100

    First commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+	public static int add(int value1, int value2) {
+		return value1 + value2;
+	}
+	public static int substract(int minuend, int subtrahend) {
+		return minuend - subtrahend;
+	}
+	public static int multiply(int value1, int value2) {
+		return value1 * value2;
+	}
+}
